2684aa8e89bbe5b8ff15c123830d7254d8a44f1e
Yoshihide Chubachi <yoshi@chubachi.net>
2013-02-16 03:22:12 +0900

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+echo '- executing middleman'
 bundle exec middleman build
+echo '- executing git check-in'
 git add .
 git commit -a -m 'deploy'
+echo '- executing git push'
 git push
 
-ssh kirin (cd /home/aiit/git/landing_page_bs/; git pull;)
+echo '- deploying'
+ssh kirin '(cd /home/aiit/git/landing_page_bs/; git pull;)'
+
+echo '- done'
